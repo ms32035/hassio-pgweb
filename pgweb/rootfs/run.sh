af56b3a8d00d6ac4710bf12b7d8d0ce65bc265f8
Marcin Szymanski <ms32035@gmail.com>
2020-06-04 23:14:22 +0100

    fix parameters

--- pgweb/rootfs/run.sh
+++ pgweb/rootfs/run.sh
@@ -5,7 +5,7 @@ CONFIG_PATH=/data/options.json
 /pgweb \
 --host $(jq --raw-output ".host" $CONFIG_PATH) \
 --port $(jq --raw-output ".port" $CONFIG_PATH) \
---username $(jq --raw-output ".username" $CONFIG_PATH) \
---password $(jq --raw-output ".password" $CONFIG_PATH) \
---database $(jq --raw-output ".database" $CONFIG_PATH) \
+--user $(jq --raw-output ".username" $CONFIG_PATH) \
+--pass $(jq --raw-output ".password" $CONFIG_PATH) \
+--db $(jq --raw-output ".database" $CONFIG_PATH) \
 --listen 8099
